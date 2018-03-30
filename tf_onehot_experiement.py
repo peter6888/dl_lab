17814f96012ca5a6d818cd006164b47f450a9442
peter6888 <qq1800@gmail.com>
2018-03-30 15:42:51 -0700

    One more tf_onehot experiement for boolean values, True->[0.,1.], False->[1., 0.]

--- tf_onehot_experiement.py
+++ tf_onehot_experiement.py
@@ -3,14 +3,28 @@ def test_onehot():
     oh = tf.one_hot([0,0,1], depth=2)
     return oh
 
+def test_onehot_boolean():
+    oh = tf.one_hot([True, False, True], depth=2)
+    return oh
+
 if __name__=="__main__":
     o_h = test_onehot()
+    ob_h = test_onehot_boolean()
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         _o = sess.run(o_h)
         print(_o)
         ''' output
         [[1. 0.]
+        [1. 0.]
+        [0. 1.]]
+        '''
+        _ob = sess.run(ob_h)
+
+        print("output shape {}".format(ob_h.get_shape()))
+        print(_ob)
+        ''' output
+        [[0. 1.]
         [1. 0.]
         [0. 1.]]
         '''

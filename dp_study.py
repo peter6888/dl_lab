a512e33091e320d51b87ab51eb45f200067e6fe0
peter6888 <qq1800@gmail.com>
2018-04-14 09:18:03 -0700

    dynamic programming study practice series (1)
    dynamic programming study practice series (1)

---
+++ dp_study.py
@@ -0,0 +1,18 @@
+'''
+The implementations when reading this article
+https://www.topcoder.com/community/data-science/data-science-tutorials/dynamic-programming-from-novice-to-advanced/
+'''
+import sys
+def q1_coins(sumto = 11, coins = [1,3,5]):
+    states = [sys.maxsize for _ in range(sumto + 1)]
+    states[0] = 0
+    for i in range(1, sumto+1): # update sumto steps
+        for coin in coins:
+            if coin <= i and states[i-coin] + 1 < states[i]:
+                states[i] = states[i-coin] + 1
+    return states[sumto]
+
+if __name__ == "__main__":
+    print("q1 coins default input:{}".format(q1_coins()))
+    print("q1 coints 20 with coins [1,5,7], output:{}".format(q1_coins(20, [1,5,7])))
+

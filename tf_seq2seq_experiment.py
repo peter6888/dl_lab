a75f3f3a151f7894a5348c61ca9cbda561aea7ab
peter6888 <qq1800@gmail.com>
2018-05-25 20:23:04 -0700

    use reuse=tf.AUTO_REUSE to reduce duplicate code
    use reuse=tf.AUTO_REUSE to reduce duplicate code

--- tf_seq2seq_experiment.py
+++ tf_seq2seq_experiment.py
@@ -16,7 +16,8 @@ def test_embed_sequence():
         ids = features,
         vocab_size = len(vocab),
         embed_dim=EMBEDDING_SIZE,
-        scope=scope_name
+        scope=scope_name,
+        reuse=tf.AUTO_REUSE
     )
 
     with tf.Session() as sess:
@@ -49,16 +50,6 @@ def test_embed_sequence():
 
 def test_read_embedding():
     print("Test read embedding from former defined embedding matrix.")
-    features = [[1,2,3,4,5], [5,6,7,8,8]]
-    vocab = [{'garbage':1}, {'piles':2}, {'in':3}, {'the':4}, {'city':5}, {'is':6}, {'clogged':7}, {'with':8}, {'vehicles':9}]
-
-    EMBEDDING_SIZE = 10
-    features_embedded = tf.contrib.layers.embed_sequence(
-        ids = features,
-        vocab_size = len(vocab),
-        embed_dim=EMBEDDING_SIZE,
-        scope=scope_name,
-        reuse=True)
 
     with tf.variable_scope(scope_name, reuse=True):
         embedding_matrix = tf.get_variable(name='embeddings')
@@ -66,9 +57,24 @@ def test_read_embedding():
     dec_embedded_input = tf.nn.embedding_lookup(embedding_matrix, dec_input)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        _f_embedded, _embedded_input = sess.run([features_embedded, dec_embedded_input])
-        print(_f_embedded[0][0], _embedded_input)
-        assert np.array_equal(_f_embedded[0][0],_embedded_input[0][0])
+        _embedded_input = sess.run(dec_embedded_input)
+        print(_embedded_input)
+''' sample output
+Test read embedding from former defined embedding matrix.
+[[[-0.47289813 -0.25815833 -0.31500334  0.24488401 -0.28925917
+    0.13878876  0.2348941   0.41908973  0.14340913  0.18852264]
+  [-0.5198182   0.52239734  0.50987273  0.5030301  -0.11493057
+    0.39601523  0.29608607  0.18276781  0.5310734  -0.39876503]
+  [-0.4901062   0.02497995  0.3326841   0.03895646  0.31780565
+   -0.17664227 -0.34979734  0.03468615  0.17545146  0.12327039]]
+
+ [[-0.3769814   0.07246828  0.29415274 -0.43250942 -0.32478
+    0.06988913  0.35327882  0.1871854   0.01241922 -0.49669534]
+  [-0.13383177  0.39311522 -0.29894897 -0.440247   -0.26188886
+    0.48903102 -0.32403827 -0.36356026  0.238002    0.00801349]
+  [ 0.0863629   0.07717121 -0.31319985  0.02371722  0.16456145
+    0.25066954 -0.48035514  0.00727534  0.3620203  -0.3014355 ]]]
+'''
 
 if __name__ == "__main__":
     test_embed_sequence()
